033ddef05f0da84d5fb157692763ac4fd33991ca
Ripp <bartekzadrozny@wp.pl>
2022-03-14 12:53:03 +0100

    Init BE project + BE Tests

---
+++ Backend/CateringBackendUnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using CateringBackEnd.Controllers;
+using Xunit;
+
+namespace CateringBackEndUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, 4);
+        }
+    }
+}
