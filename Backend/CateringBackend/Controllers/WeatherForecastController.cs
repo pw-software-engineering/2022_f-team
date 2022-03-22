3eed1b08866c6fcb00beff82d967bf9a1c226755
Ripp <bartekzadrozny@wp.pl>
2022-03-22 19:53:08 +0100

    WeatherForecast cleanup

--- Backend/CateringBackend/Controllers/WeatherForecastController.cs
+++ Backend/CateringBackend/Controllers/WeatherForecastController.cs
@@ -1,11 +1,5 @@
-﻿using CateringBackend.Utilities.Enums;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CateringBackEnd.Controllers
 {
