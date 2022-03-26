c18a1378e3bc46627fb992cedf3c5834d54d862a
Jakub Tarkowski <45311300+jtarkowski27@users.noreply.github.com>
2022-03-26 10:45:05 +0100

    Create common-components UI react library

---
+++ Frontend/common-components/example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'common-components'
+import 'common-components/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
