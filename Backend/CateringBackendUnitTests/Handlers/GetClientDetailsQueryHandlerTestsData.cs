1adb8fd444b1f567d4888fbcd6bd871c8d58e4ea
Wojtek <wojtekulaski@gmail.com>
2022-03-29 17:27:42 +0200

    getClientDetailsTests

---
+++ Backend/CateringBackendUnitTests/Handlers/GetClientDetailsQueryHandlerTestsData.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using CateringBackend.Domain.Entities;
+using Microsoft.VisualBasic;
+
+namespace CateringBackendUnitTests.Handlers
+{
+    public static class GetClientDetailsQueryHandlerTestsData
+    {
+
+        public static IEnumerable<object[]> GetValidClients()
+        {
+            var firstClientAddress = new Guid("f07190b2-9cce-4df7-b22f-2e5be14a7066");
+            yield return new object[]
+            {
+                new Client
+                {
+                    Address = new Address
+                    {
+                        Id = firstClientAddress,
+                        ApartmentNumber = "123",
+                        Street = "ClientStreet",
+                        City = "ClientCity",
+                        PostCode = "ClientPostCode",
+                        BuildingNumber = "ClientBuildingNumber"
+                    },
+                    Email = "ClientEmail@gmail.com",
+                    FirstName = "ClientFirstName",
+                    LastName = "ClientLastName",
+                    Password = "asdfasdf",
+                    PhoneNumber = "123456789",
+                    AddressId = firstClientAddress
+                }
+            };
+
+            var secondClientAddress = new Guid("48aef421-6685-4fab-95d3-66dfb09c34c5");
+            yield return new object[]
+            {
+                new Client
+                {
+                    Address = new Address
+                    {
+                        Id = secondClientAddress,
+                        ApartmentNumber = "321",
+                        Street = "ClientStreet2",
+                        City = "ClientCity2",
+                        PostCode = "ClientPostCode2",
+                        BuildingNumber = "ClientBuildingNumber2"
+                    },
+                    Email = "ClientEmail2@gmail.com",
+                    FirstName = "ClientFirstName2",
+                    LastName = "ClientLastName2",
+                    Password = "asdfasdf2",
+                    PhoneNumber = "987654321",
+                    AddressId = secondClientAddress
+                }
+            };
+        }
+    }
+}
