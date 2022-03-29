1f6e228642a75e5767f8b02ec047e084ac30dd3e
Wojtek <wojtekulaski@gmail.com>
2022-03-29 17:29:09 +0200

    remove unused imports

--- Backend/CateringBackendUnitTests/Handlers/GetClientDetailsQueryHandlerTestsData.cs
+++ Backend/CateringBackendUnitTests/Handlers/GetClientDetailsQueryHandlerTestsData.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using CateringBackend.Domain.Entities;
-using Microsoft.VisualBasic;
 
 namespace CateringBackendUnitTests.Handlers
 {
