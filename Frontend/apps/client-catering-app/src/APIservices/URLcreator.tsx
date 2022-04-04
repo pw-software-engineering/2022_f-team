deff1e9259cbd7c4ffdb5fc36363439d841b9058
Wojtek <wojtekulaski@gmail.com>
2022-04-04 23:24:48 +0200

    enable cors and establish BE/FE connection

--- Frontend/apps/client-catering-app/src/APIservices/URLcreator.tsx
+++ Frontend/apps/client-catering-app/src/APIservices/URLcreator.tsx
@@ -1,3 +1,3 @@
-const apiUrl = 'https://localhost:5000';
+const apiUrl = "https://localhost:5001";
 
-export const getRegisterClientURL = ():string => apiUrl+'/Client/register';
+export const getRegisterClientURL = (): string => apiUrl + "/Client/register";
