77a662391bd03dcc6d99343b4c6ada7304c35c25
Ripp <bartekzadrozny@wp.pl>
2022-04-05 21:28:21 +0200

    Bug fix

--- Backend/CateringBackend/Meals/Queries/GetMealDetailsQuery.cs
+++ Backend/CateringBackend/Meals/Queries/GetMealDetailsQuery.cs
@@ -25,7 +25,7 @@ namespace CateringBackend.Meals.Queries
             Name = meal.Name;
             IngredientList = meal.Ingredients.SplitByCommaToArray();
             AllergenList = meal.Allergens.SplitByCommaToArray();
-            Calories = meal.Calories;
+            Calories = (int)meal.Calories;
             Vegan = meal.IsVegan;
         }
     }
