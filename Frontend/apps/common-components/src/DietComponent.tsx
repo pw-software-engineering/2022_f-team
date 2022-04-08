80d89f4b93e37891d280b307aad480fe63954edf
ZuzaTwardowska <51333262+ZuzaTwardowska@users.noreply.github.com>
2022-04-08 12:13:06 +0200

    add grid styling

--- Frontend/apps/common-components/src/DietComponent.tsx
+++ Frontend/apps/common-components/src/DietComponent.tsx
@@ -1,20 +1,32 @@
 import React from 'react'
-import ExpandMoreButton from './ExpandMoreButton';
+import { useState } from 'react'
+import ExpandMoreButton from './ExpandMoreButton'
 
 const DietComponent = () => {
-    return (
-      <div className="diet-div">
-        <h1>DietName</h1>
-        <p>Description Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  const [showMeals, setShowMeals] = useState<boolean>(false)
+  const toogleShowMeals = (): void => setShowMeals(!showMeals)
+
+  return (
+    <div className='diet-div'>
+      <h1>DietName</h1>
+      <p>
+        Description Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+        do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+        in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+        officia deserunt mollit anim id est laborum.
+      </p>
+      <div className='calories-price-div'>
         <p>Calories: xxx</p>
         <p>Price: xxx</p>
-        <button>Add to cart</button>
-        <ExpandMoreButton />
-        <div>
-            Meals
-        </div>
+        <button className='addToCartButton'>Add to cart</button>
       </div>
-    );
-  };
-  
-  export default DietComponent;
+      <ExpandMoreButton onClick={toogleShowMeals} />
+      {showMeals && <div>Meals</div>}
+    </div>
+  )
+}
+
+export default DietComponent
--- Frontend/apps/common-components/src/ExpandMoreButton.tsx
+++ Frontend/apps/common-components/src/ExpandMoreButton.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
-const ExpandMoreButton = () => {
-    return (
-      <button>Expand <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M24 24H0V0h24v24z" fill="none" opacity=".87"/><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6-1.41-1.41z"/></svg></button>
-    );
-  };
-  
-  export default ExpandMoreButton;
+interface ExpandMoreButtonProps {
+  onClick: () => void
+}
+
+const ExpandMoreButton = (props: ExpandMoreButtonProps) => {
+  return (
+    <button className='expandMoreButton' onClick={props.onClick}>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        height='48px'
+        viewBox='0 0 24 24'
+        width='48px'
+        fill='#000000'
+      >
+        <path d='M24 24H0V0h24v24z' fill='none' opacity='.87' />
+        <path d='M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6-1.41-1.41z' />
+      </svg>
+    </button>
+  )
+}
+
+export default ExpandMoreButton
