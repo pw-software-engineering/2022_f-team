90b64d7d0e00877bfe76ef4639f18d870d0d296b
Ripp <bartekzadrozny@wp.pl>
2022-04-10 16:11:41 +0200

    Delete meal endpoint

---
+++ Backend/CateringBackend/Meals/Commands/DeleteMealCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CateringBackend.Domain.Data;
+using CateringBackend.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace CateringBackend.Meals.Queries
+{
+    public record DeleteMealCommand(Guid MealId) : IRequest<Meal>;
+
+    public class DeleteMealCommandHandler : IRequestHandler<DeleteMealCommand, Meal>
+    {
+        private readonly CateringDbContext _dbContext;
+
+        public DeleteMealCommandHandler(CateringDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Meal> Handle(DeleteMealCommand request, CancellationToken cancellationToken)
+        {
+            var meal = await _dbContext.Meals.FirstOrDefaultAsync(meal => meal.Id == request.MealId);
+            if (meal == default)
+                return null;
+
+            if (await MealWithGivenIdIsContaiendByDiet(request.MealId))
+                return meal;
+
+            meal.MakeUnavailable();
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return meal;
+        }
+
+        private async Task<bool> MealWithGivenIdIsContaiendByDiet(Guid mealId) =>
+        await _dbContext.Diets.AnyAsync(diet => diet.Meals.Any(meal => meal.Id == mealId));
+    }
+}
