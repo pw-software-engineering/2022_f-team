f79c14c039c2437b3c798151e39a5dfa537b5759
ZuzaTwardowska <51333262+ZuzaTwardowska@users.noreply.github.com>
2022-04-10 12:12:26 +0200

    add and position icons

--- Frontend/apps/client-catering-app/src/App.tsx
+++ Frontend/apps/client-catering-app/src/App.tsx
@@ -3,10 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import LoginPage from "./pages/LoginPage";
 import DietListPage from "./pages/DietListPage";
+import {CartIcon, MyProfileIcon } from "common-components"
+import './style/NavbarStyle.css'
 
 const App = () => {
   return (
     <BrowserRouter>
+    <CartIcon/>
+    <MyProfileIcon />
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/register" element={<RegisterPage />} />
