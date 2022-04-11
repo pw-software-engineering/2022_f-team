d2fd69eee45e63cc5b03d7cd09262d687d908640
ZuzaTwardowska <51333262+ZuzaTwardowska@users.noreply.github.com>
2022-04-12 00:40:30 +0200

    import css in deliverer and producer

--- Frontend/apps/deliverer-catering-app/src/App.tsx
+++ Frontend/apps/deliverer-catering-app/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import LoginPage from "./pages/LoginPage";
 import { Logo } from "common-components";
+import "./style/NavbarStyle.css"
 
 const App = () => {
   return (
