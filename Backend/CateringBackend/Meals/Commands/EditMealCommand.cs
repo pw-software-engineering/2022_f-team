fb933b8ae6006d814ccac2aba35a0a2921e8cd1d
Ripp <bartekzadrozny@wp.pl>
2022-04-13 13:36:26 +0200

    Add edit meal endpoint

---
+++ Backend/CateringBackend/Meals/Commands/EditMealCommand.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CateringBackend.Domain.Data;
+using CateringBackend.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace CateringBackend.Meals.Commands
+{
+    public class EditMealCommand : IRequest<Meal>
+    {
+        public Guid MealId { get; set; }
+        public string Name { get; set; }
+        public string[] IngredientList { get; set; }
+        public string[] AllergenList { get; set; }
+        public int Calories { get; set; }
+        public bool Vegan { get; set; }
+    }
+
+    public class EditMealCommandHandler : IRequestHandler<EditMealCommand, Meal>
+    {
+        private readonly CateringDbContext _dbContext;
+
+        public EditMealCommandHandler(CateringDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Meal> Handle(EditMealCommand request, CancellationToken cancellationToken)
+        {
+            var mealToEdit = await SearchForMealInDatabase(request.MealId, cancellationToken);
+
+            if (mealToEdit == default)
+                return null;
+
+            mealToEdit.MakeUnavailable();
+
+            var newMeal = await AddMealToDatabaseAsync(request, cancellationToken);
+            
+            var dietsToEdit = SearchDietsContainingMealId(mealToEdit.Id);
+            await dietsToEdit.ForEachAsync(d => d.MakeUnavailable(), cancellationToken);
+
+            await AddDietsWithEditedMealToDatabaseAsync(dietsToEdit, mealToEdit, newMeal, cancellationToken);
+            
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return mealToEdit;
+        }
+
+        private async Task<Meal> SearchForMealInDatabase(Guid mealId, CancellationToken cancellationToken) => await
+            _dbContext.Meals.Where(meal => meal.IsAvailable).FirstOrDefaultAsync(meal => meal.Id == mealId, cancellationToken);
+        private async Task<Meal> AddMealToDatabaseAsync(EditMealCommand addMealCommand, CancellationToken cancellationToken)
+        {
+            var createdMeal = await _dbContext.Meals.AddAsync(
+                Meal.Create(
+                    addMealCommand.Name,
+                    string.Join(',', addMealCommand.IngredientList),
+                    string.Join(',', addMealCommand.AllergenList),
+                    addMealCommand.Calories,
+                    addMealCommand.Vegan), 
+                cancellationToken);
+            return createdMeal.Entity;
+        }
+
+        private async Task AddDietsWithEditedMealToDatabaseAsync(IQueryable<Diet> dietsToEdit, Meal mealToEdit, Meal newMeal, CancellationToken cancellationToken)
+        {
+            await dietsToEdit.ForEachAsync(async d => await _dbContext.Diets.AddAsync(
+                    Diet.Create(
+                        d.Title,
+                        d.Description,
+                        d.Price,
+                        ReplaceEditedMeal(d.Meals, mealToEdit, newMeal)),
+                    cancellationToken), 
+                cancellationToken);
+        }
+
+        private IEnumerable<Meal> ReplaceEditedMeal(IEnumerable<Meal> meals, Meal mealToEdit, Meal newMeal) => 
+            meals.Where(m => m.Id == mealToEdit.Id).Select(m => newMeal);
+
+        private IQueryable<Diet> SearchDietsContainingMealId(Guid mealId) =>
+            _dbContext.Diets.Where(d => d.IsAvailable && d.Meals.Any(m => m.Id == mealId));
+    }
+}
