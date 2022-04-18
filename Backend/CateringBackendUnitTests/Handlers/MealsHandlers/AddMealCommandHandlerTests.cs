240512f35b6d7ffafe523fb78054646d319c24fc
Ripp <bartekzadrozny@wp.pl>
2022-04-18 20:15:20 +0200

    Post meal UT

---
+++ Backend/CateringBackendUnitTests/Handlers/MealsHandlers/AddMealCommandHandlerTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CateringBackend.Domain.Data;
+using CateringBackend.Domain.Entities;
+using CateringBackend.Meals.Commands;
+using EntityFrameworkCore.Testing.Moq;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace CateringBackendUnitTests.Handlers.MealsHandlers
+{
+    public class AddMealCommandHandlerTests
+    {
+        private readonly CateringDbContext _dbContext;
+
+        public AddMealCommandHandlerTests()
+        {
+            var options = new DbContextOptionsBuilder<CateringDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+            _dbContext = Create.MockedDbContextFor<CateringDbContext>(options);
+        }
+
+        [Fact]
+        public async Task GivenMealWithNameThatIsUsedByAvailableMeal_WhenHandleAddMealCommand_ThenReturnsFalse()
+        {
+            // Arrange
+            const string testMealName = "testMeal";
+
+            _dbContext.Meals.Add(new Meal
+            {
+                Name = testMealName,
+                IsAvailable = true,
+            });
+            await _dbContext.SaveChangesAsync();
+
+            var addMealCommandHandler = new AddMealCommandHandler(_dbContext);
+            var addMealCommand = new AddMealCommand
+            {
+                Name = testMealName
+            };
+
+            // Act
+            var result = await addMealCommandHandler.Handle(addMealCommand, CancellationToken.None);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Theory]
+        [MemberData(nameof(AddMealCommandHandlerTestsData.GetValidAddMealCommands),
+            MemberType = typeof(AddMealCommandHandlerTestsData))]
+        public async Task GivenAddMealCommand_WhenHandle_ThenAddsCorrectMealToDatabase(
+            AddMealCommand addMealCommand)
+        {
+            // Arrange
+            var addMealCommandHandler = new AddMealCommandHandler(_dbContext);
+
+            // Act
+            await addMealCommandHandler.Handle(addMealCommand, CancellationToken.None);
+
+            // Assert
+            AssertMealAddedToDatabase(_dbContext, addMealCommand);
+        }
+
+        [Theory]
+        [MemberData(nameof(AddMealCommandHandlerTestsData.GetValidAddMealCommands),
+            MemberType = typeof(AddMealCommandHandlerTestsData))]
+        public async Task GivenAddMealCommand_WhenHandle_ThenReturnsTrue(
+            AddMealCommand addMealCommand)
+        {
+            // Arrange
+            var addMealCommandHandler = new AddMealCommandHandler(_dbContext);
+
+            // Act
+            var result = await addMealCommandHandler.Handle(addMealCommand, CancellationToken.None);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        private static void AssertMealAddedToDatabase(CateringDbContext dbContext,
+            AddMealCommand addMealCommand)
+        {
+            Assert.Equal(1, dbContext.Meals.Count());
+            var addedMeal = dbContext.Meals.First();
+            Assert.Equal(addMealCommand.Name, addedMeal.Name);
+            Assert.Equal(addMealCommand.Calories, addedMeal.Calories);
+            Assert.Equal(addMealCommand.Vegan, addedMeal.IsVegan);
+
+            if (addMealCommand.IngredientList != null)
+                Assert.Equal(string.Join(',', addMealCommand.IngredientList), addedMeal.Ingredients);
+            else
+                Assert.Equal(string.Empty, addedMeal.Ingredients);
+
+            if (addMealCommand.AllergenList != null)
+                Assert.Equal(string.Join(',', addMealCommand.AllergenList), addedMeal.Allergens);
+            else
+                Assert.Equal(string.Empty, addedMeal.Allergens);
+        }
+    }
+}
---
+++ Backend/CateringBackendUnitTests/Handlers/MealsHandlers/AddMealCommandHandlerTestsData.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using CateringBackend.Meals.Commands;
+
+namespace CateringBackendUnitTests.Handlers.MealsHandlers
+{
+    public class AddMealCommandHandlerTestsData
+    {
+        public static IEnumerable<object[]> GetValidAddMealCommands()
+        {
+            yield return new object[]
+            {
+                new AddMealCommand
+                {
+                    Name = "testMeal1",
+                    Calories = 250,
+                    Vegan = true,
+                    IngredientList = new string[]
+                    {
+                        "Kurczak",
+                        "Frytki"
+                    },
+                    AllergenList = new string[]
+                    {
+                        "Ziemniak",
+                        "Orzechy"
+                    }
+                }
+            };
+
+            yield return new object[]
+            {
+                new AddMealCommand
+                {
+                    Name = "testMeal2",
+                    Calories = 250,
+                    Vegan = true,
+                    IngredientList = new string[] { },
+                    AllergenList = new string[] { }
+                }
+            };
+
+            yield return new object[]
+{
+                new AddMealCommand
+                {
+                    Name = "testMeal2",
+                    Calories = 250,
+                    Vegan = true
+                }
+};
+        }
+    }
+}
