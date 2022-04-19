171f4cecc6c882bb14065c3d1d837809fb0f5064
ZuzaTwardowska <51333262+ZuzaTwardowska@users.noreply.github.com>
2022-04-19 17:41:34 +0200

    connect get diets

--- Frontend/apps/client-catering-app/src/Services/URLcreator.tsx
+++ Frontend/apps/client-catering-app/src/Services/URLcreator.tsx
@@ -3,3 +3,5 @@ const apiUrl = "https://localhost:5001";
 export const getRegisterClientURL = (): string => apiUrl + "/Client/register";
 
 export const getLoginClientURL = (): string => apiUrl + "/Client/login";
+
+export const getDietsURL = (): string => apiUrl + "/Diets";
