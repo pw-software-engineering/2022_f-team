afd91c44e5266df0eeb61897e95dc0aba37523a1
Ripp <bartekzadrozny@wp.pl>
2022-05-05 19:39:00 +0200

    cleanup

--- Backend/CateringBackend/Diets/Commands/DeleteDietCommand.cs
+++ Backend/CateringBackend/Diets/Commands/DeleteDietCommand.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using CateringBackend.Domain.Data;
-using CateringBackend.Domain.Entities;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
