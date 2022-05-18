96375ed4b4580cd4477272cd473250b977da1b75
Bartosz Tomala <bartosz.tomala@ascron.com>
2022-05-18 19:17:28 +0200

    add deliverer implemnetation

---
+++ Backend/CateringBackend/Users/Deliverer/Queries/GetDelivererOrdersQuery.cs
@@ -0,0 +1,31 @@
+﻿using CateringBackend.Domain.Data;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CateringBackend.Users.Deliverer.Queries
+{
+    public record GetDelivererOrdersQuery() : IRequest<IEnumerable<OrderDeliveryDetailsDTO>>;
+    public class GetDelivererOrdersQueryHandler : IRequestHandler<GetDelivererOrdersQuery, IEnumerable<OrderDeliveryDetailsDTO>>
+    {
+        private readonly CateringDbContext _dbContext;
+
+        public GetDelivererOrdersQueryHandler(CateringDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<OrderDeliveryDetailsDTO>> Handle(GetDelivererOrdersQuery request, CancellationToken cancellationToken)
+        {
+            var orders = await _dbContext.Orders
+                .Where(x => x.Status == Domain.Entities.Enums.OrderStatus.Prepared)
+                .Select(x => new OrderDeliveryDetailsDTO(x))
+                .ToListAsync(cancellationToken);
+
+            return orders;
+        }
+    }
+}
---
+++ Backend/CateringBackend/Users/OrderDeliveryDetailsDTO.cs
@@ -0,0 +1,22 @@
+﻿using CateringBackend.Domain.Entities;
+using System;
+
+namespace CateringBackend.Users
+{
+    public class OrderDeliveryDetailsDTO
+    {
+        public Guid OrderId { get; set; }
+        public AddressDTO Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string CommentForDeliverer { get; set; }
+
+        public OrderDeliveryDetailsDTO() { }
+        public OrderDeliveryDetailsDTO(Order order)
+        {
+            OrderId = order.Id;
+            Address = new AddressDTO(order.Client.Address);
+            PhoneNumber = order.Client.PhoneNumber;
+            CommentForDeliverer = order.CommentForDeliverer;
+        }
+    }
+}
