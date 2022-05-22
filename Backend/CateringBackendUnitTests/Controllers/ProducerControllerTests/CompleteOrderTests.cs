2e2c44c946ddc64341d3b383ecd61acc610fd9b5
Ripp <bartekzadrozny@wp.pl>
2022-05-22 19:24:44 +0200

    POST complete order UT

---
+++ Backend/CateringBackendUnitTests/Controllers/ProducerControllerTests/CompleteOrderTests.cs
@@ -0,0 +1,62 @@
+﻿using CateringBackend.Controllers;
+using CateringBackend.Users.Producer.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Moq;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CateringBackendUnitTests.Controllers.ProducerControllerTests
+{
+    public class CompleteOrderTests
+    {
+        private readonly ProducerController _producersController;
+        private readonly Mock<IMediator> _mockedMediator;
+
+        public CompleteOrderTests()
+        {
+            _mockedMediator = new Mock<IMediator>();
+            _producersController = new ProducerController(_mockedMediator.Object);
+        }
+
+        [Fact]
+        public async void GivenCompleteOrderCommand_WhenCompleteOrder_ThenItIsSentToMediator()
+        {
+            // Arrange
+            _mockedMediator
+                .Setup(x => x.Send(It.IsAny<CompleteOrderCommand>(), It.IsAny<CancellationToken>()))
+                .Verifiable("Complete order command was not sent");
+
+            // Act 
+            await _producersController.CompleteOrder(Guid.NewGuid());
+
+            // Assert
+            _mockedMediator.Verify(x => x.Send(It.IsAny<CompleteOrderCommand>(), It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Theory]
+        [InlineData(false, false, HttpStatusCode.NotFound)]
+        [InlineData(true, false, HttpStatusCode.BadRequest)]
+        [InlineData(true, true, HttpStatusCode.OK)]
+        public async void GivenMediatorReturnValue_WhenCompleteOrder_ThenReturnsProperHttpStatusCode(
+            bool orderExists, bool orderCompleted, HttpStatusCode expectedStatusCode)
+        {
+            // Arrange
+            _mockedMediator
+                .Setup(x => x.Send(It.IsAny<CompleteOrderCommand>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult((orderExists, orderCompleted)))
+                .Verifiable("Complete order command was not sent");
+
+            // Act 
+            var result = await _producersController.CompleteOrder(Guid.NewGuid());
+            var statusCodeActionResult = result as IStatusCodeActionResult;
+
+            // Assert
+            Assert.NotNull(statusCodeActionResult);
+            Assert.Equal((int)expectedStatusCode, statusCodeActionResult.StatusCode);
+        }
+    }
+}
---
+++ Backend/CateringBackendUnitTests/Handlers/ProducerHandlers/CompleteOrderCommandHandlerTests.cs
@@ -0,0 +1,86 @@
+﻿using CateringBackend.Domain.Data;
+using CateringBackend.Domain.Entities.Enums;
+using CateringBackend.Users.Producer.Commands;
+using EntityFrameworkCore.Testing.Moq;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CateringBackendUnitTests.Handlers.ProducerHandlers
+{
+    public class CompleteOrderCommandHandlerTests
+    {
+        private readonly CateringDbContext _dbContext;
+        private readonly CompleteOrderCommandHandler _completeOrderCommandHandler;
+
+        public CompleteOrderCommandHandlerTests()
+        {
+            var options = new DbContextOptionsBuilder<CateringDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+            _dbContext = Create.MockedDbContextFor<CateringDbContext>(options);
+            _completeOrderCommandHandler = new CompleteOrderCommandHandler(_dbContext);
+        }
+
+        [Fact]
+        public async Task GivenOrderIdThatNotExists_WhenHandle_ThenReturnsOrderExistsAsFalse()
+        {
+            // Act
+            var result = await _completeOrderCommandHandler.Handle(new CompleteOrderCommand(Guid.NewGuid()), CancellationToken.None);
+
+            // Assert
+            Assert.False(result.orderExists);
+            Assert.False(result.orderCompleted);
+        }
+
+        [Fact]
+        public async Task GivenOrderThatIsNotToRealized_WhenHandle_ThenReturnsOrderExistsAsFalse()
+        {
+            // Arrange
+            var orderId = Guid.NewGuid();
+            _dbContext.Orders.Add(new() { Id = orderId, Status = OrderStatus.WaitingForPayment });
+            await _dbContext.SaveChangesAsync();
+
+            // Act
+            var result = await _completeOrderCommandHandler.Handle(new CompleteOrderCommand(orderId), CancellationToken.None);
+
+            // Assert
+            Assert.False(result.orderExists);
+            Assert.False(result.orderCompleted);
+        }
+
+        [Fact]
+        public async Task GivenOrderThatIsToRealized_WhenHandle_ThenReturnsOrderExistsAndOrderCompletedAsTrue()
+        {
+            // Arrange
+            var orderId = Guid.NewGuid();
+            _dbContext.Orders.Add(new() { Id = orderId, Status = OrderStatus.ToRealized });
+            await _dbContext.SaveChangesAsync();
+
+            // Act
+            var result = await _completeOrderCommandHandler.Handle(new CompleteOrderCommand(orderId), CancellationToken.None);
+
+            // Assert
+            Assert.True(result.orderExists);
+            Assert.True(result.orderCompleted);
+        }
+
+        [Fact]
+        public async Task GivenOrderThatIsToRealized_WhenHandle_ThenChangesOrderStatusToPreparedInDatabase()
+        {
+            // Arrange
+            var orderId = Guid.NewGuid();
+            _dbContext.Orders.Add(new() { Id = orderId, Status = OrderStatus.ToRealized });
+            await _dbContext.SaveChangesAsync();
+
+            // Act
+            var result = await _completeOrderCommandHandler.Handle(new CompleteOrderCommand(orderId), CancellationToken.None);
+
+            // Assert
+            var order = _dbContext.Orders.First();
+            Assert.Equal(OrderStatus.Prepared, order.Status);
+        }
+    }
+}
