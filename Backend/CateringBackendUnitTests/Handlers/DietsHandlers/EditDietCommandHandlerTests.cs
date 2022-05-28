bb664742432182b6b40d31f2e9347d88e944ff8a
Wojtazooo <33081464+Wojtazooo@users.noreply.github.com>
2022-05-28 21:34:08 +0200

    feature/16-diets-UT (#101)
    * tests
    
    * .

---
+++ Backend/CateringBackendUnitTests/Handlers/DietsHandlers/EditDietCommandHandlerTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using CateringBackend.Diets.Commands;
+using CateringBackend.Diets.Queries;
+using CateringBackend.Domain.Data;
+using CateringBackend.Domain.Entities;
+using EntityFrameworkCore.Testing.Moq;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xunit;
+
+namespace CateringBackendUnitTests.Handlers.DietsHandlers
+{
+    public class EditDietCommandHandlerTests
+    {
+        private readonly EditDietWithDietIdCommandHandler _editDietCommandHandler;
+        private readonly CateringDbContext _dbContext;
+        private readonly Mock<IMediator> _mockedMediator;
+
+        public EditDietCommandHandlerTests()
+        {
+            var options = new DbContextOptionsBuilder<CateringDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+            _dbContext = Create.MockedDbContextFor<CateringDbContext>(options);
+            _mockedMediator = new Mock<IMediator>();
+            _editDietCommandHandler = new EditDietWithDietIdCommandHandler(_mockedMediator.Object, _dbContext);
+        }
+
+        [Fact]
+        public async Task GivenEditDietCommandForNotExistingDiet_WhenHandle_ThenReturnsDietExistsAndEditedAsFalse()
+        {
+            // Act
+            var result = await _editDietCommandHandler.Handle(new EditDietWithDietIdCommand(new EditDietCommand(), Guid.NewGuid()),
+                CancellationToken.None);
+
+            // Assert
+            Assert.False(result.dietExists);
+            Assert.False(result.dietEdited);
+        }
+
+        [Fact]
+        public async Task GivenEditDietCommandForExistingDiet_WhenHandle_ThenInvokesMediatorWithAddDiet()
+        {
+            // Arrange
+            var dietInDatabase = new Diet
+            {
+                Description = "myDescription",
+                Id = Guid.NewGuid(),
+                IsAvailable = true,
+                Meals = new HashSet<Meal>
+                {
+                    new() {Name = "meal1", Id = Guid.NewGuid(), Calories = 10, IsVegan = true},
+                    new() {Name = "meal2", Id = Guid.NewGuid(), Calories = 20, IsVegan = false},
+                },
+                Title = "myDiet",
+                Price = 1
+            };
+
+            _dbContext.Diets.Add(dietInDatabase);
+            await _dbContext.SaveChangesAsync();
+
+            _mockedMediator
+                .Setup(x => x.Send(It.IsAny<AddDietCommand>(), It.IsAny<CancellationToken>()))
+                .Verifiable("AddDietCommand was not sent");
+
+            var editDietCommand = new EditDietCommand
+            {
+                Description = "AnotherSuperDescription",
+                MealIds = new[] {dietInDatabase.Meals.First().Id},
+                Name = "AnotherNameForDiet",
+                Price = 2
+            };
+
+            // Act 
+            await _editDietCommandHandler.Handle(new EditDietWithDietIdCommand(editDietCommand, dietInDatabase.Id), CancellationToken.None);
+
+            // Assert
+            _mockedMediator.Verify(
+                x => x.Send(
+                    It.Is<AddDietCommand>(x =>
+                        x.MealIds == editDietCommand.MealIds && x.Name == editDietCommand.Name &&
+                        x.Description == editDietCommand.Description && x.Price == editDietCommand.Price),
+                    It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+    }
+}
---
+++ Backend/CateringBackendUnitTests/Handlers/DietsHandlers/GetDietDetailsQueryHandlerTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using CateringBackend.Diets.Queries;
+using CateringBackend.Domain.Data;
+using CateringBackend.Domain.Entities;
+using CateringBackend.Meals.Queries;
+using EntityFrameworkCore.Testing.Moq;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace CateringBackendUnitTests.Handlers.DietsHandlers
+{
+    public class GetDietDetailsQueryHandlerTests
+    {
+        private readonly CateringDbContext _dbContext;
+        private readonly GetDietDetailsQueryHandler _getDietDetailsQueryHandler;
+
+        public GetDietDetailsQueryHandlerTests()
+        {
+            var options = new DbContextOptionsBuilder<CateringDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+            _dbContext = Create.MockedDbContextFor<CateringDbContext>(options);
+            _getDietDetailsQueryHandler = new GetDietDetailsQueryHandler(_dbContext);
+        }
+
+        [Fact]
+        public async Task GivenQueryForExistingDiet_WhenHandleGetDietDetailsQuery_ThenReturnsValidDTO()
+        {
+            // Arrange
+            var dietInDatabase = new Diet
+            {
+                Description = "myDescription",
+                Id = Guid.NewGuid(),
+                IsAvailable = true,
+                Meals = new HashSet<Meal>
+                {
+                    new() {Name = "meal1", Id = Guid.NewGuid(), Calories = 10, IsVegan = true}, 
+                    new() {Name = "meal2", Id = Guid.NewGuid(), Calories = 20, IsVegan = false},
+                },
+                Title = "myDiet",
+                Price = 100
+            };
+
+            _dbContext.Diets.Add(dietInDatabase);
+            await _dbContext.SaveChangesAsync();
+
+            // Act
+            var result =
+                await _getDietDetailsQueryHandler.Handle(new GetDietDetailsQuery(dietInDatabase.Id),
+                    CancellationToken.None);
+
+            // Assert
+            Assert.Equal(dietInDatabase.Title, result.Name);
+            Assert.Equal(dietInDatabase.Meals.Sum(x => x.Calories), result.Calories);
+            Assert.Equal(dietInDatabase.Meals.All(x => x.IsVegan), result.Vegan);
+            Assert.Equal(dietInDatabase.Meals.Count, result.Meals.Count());
+            Assert.Equal(dietInDatabase.Price, result.Price);
+        }
+
+        [Fact]
+        public async Task GivenQueryForNotExistingDiet_WhenHandleGetDietDetailsQuery_ThenReturnsNull()
+        {
+            // Act 
+            var result =
+                await _getDietDetailsQueryHandler.Handle(new GetDietDetailsQuery(Guid.NewGuid()),
+                    CancellationToken.None);
+
+            //Assert
+            Assert.Null(result);
+        }
+
+    }
+}
