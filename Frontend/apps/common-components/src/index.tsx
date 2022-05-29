902c626ddfdfb56cf5eb34fd7ef240d64e01b722
Jakub Tarkowski <45311300+jtarkowski27@users.noreply.github.com>
2022-05-30 01:15:47 +0200

    Add icon which directs to ingredients

--- Frontend/apps/common-components/src/index.tsx
+++ Frontend/apps/common-components/src/index.tsx
@@ -18,6 +18,7 @@ export { default as CartIcon } from './Atoms/CartIcon'
 export { default as MyProfileIcon } from './Atoms/MyProfileIcon'
 
 export { default as LogoutIcon } from './Atoms/LogoutIcon'
+export { default as IngredientIcon } from './Atoms/IngredientIcon'
 
 export { default as Logo } from './Atoms/Logo'
 
---
+++ Frontend/apps/producer-catering-app/src/pages/IngredientsPage.tsx
@@ -0,0 +1,56 @@
+import {
+    UserContext,
+    DietList,
+    EditDietDialog,
+    DietModel,
+    ServiceState,
+} from "common-components";
+import { useContext, useEffect, useState } from "react";
+import "../style/DietComponentStyle.css";
+import "../style/Pagination.css";
+import "../style/Filter.css";
+import "../style/NavbarStyle.css";
+import { EditDietModel } from "common-components/dist/models/DietModel";
+import { APIservice } from "../Services/APIservice";
+import { putDietDetailsConfig } from "../Services/configCreator";
+
+interface IngredientsPageProps {
+    // AddToCart: (item: string) => void;
+}
+
+const IngredientsPage = (props: IngredientsPageProps) => {
+    const service = APIservice();
+
+    const userContext = useContext(UserContext);
+    const [showModal, setShowModal] = useState<boolean>(false)
+    const [diet, setDiet] = useState<DietModel | null>(null);
+
+    const openDialog = (value: string, diet: DietModel) => {
+        setDiet(diet);
+        setShowModal(true);
+    }
+
+    const onSubmitDiet = (diet: EditDietModel) => {
+        service.execute!(putDietDetailsConfig(userContext?.authApiKey!, diet.id),
+            diet,
+        );
+        setShowModal(false);
+    }
+
+    return (
+        <div>
+            {showModal && diet != null && (
+                <EditDietDialog
+                    closeModal={setShowModal}
+                    userContext={userContext}
+                    onSubmit={onSubmitDiet}
+                    diet={diet} />
+            )}
+            <DietList onDietButtonClick={openDialog}
+                userContext={userContext}
+                dietButtonLabel={'Edit diet'} />
+        </div>
+    );
+};
+
+export default IngredientsPage;
