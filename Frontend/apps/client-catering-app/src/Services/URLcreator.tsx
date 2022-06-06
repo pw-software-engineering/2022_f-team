1a4fb9ed23a3e844f1fb44135fdb5d10ae724533
Wojtazooo <33081464+Wojtazooo@users.noreply.github.com>
2022-06-06 14:39:32 +0200

    Feature/deploy fe (#109)
    * init
    
    * fix
    
    * .
    
    * change port
    
    * rename dockerfile
    
    * .
    
    * correct nginx path
    
    * client docker file
    
    * react router fix
    
    * change nginx
    
    * add /api to backend
    
    * add be home controller
    
    * nginx for be
    
    * ,
    
    * revert dockerfile
    
    * revert nginx
    
    * database connection string from github secret
    
    * .
    
    * .
    
    * base monitor cluster pipeline
    
    * Create monitoring.yml
    
    * extend monitoring
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * frontend Url
    
    * producer and deliverer
    
    * missing dependencies in FE
    
    * urls
    
    * .
    
    * .
    
    * revert api url
    
    * rever /api
    
    * .
    
    * .
    
    * .
    
    * gitignore fix

--- Frontend/apps/client-catering-app/src/Services/URLcreator.tsx
+++ Frontend/apps/client-catering-app/src/Services/URLcreator.tsx
@@ -1,4 +1,4 @@
-const apiUrl = "https://localhost:5001";
+const apiUrl = process.env.REACT_APP_API_URL;
 
 export const getRegisterClientURL = (): string => apiUrl + "/Client/register";
 
--- Frontend/apps/common-components/src/Services/URLcreator.tsx
+++ Frontend/apps/common-components/src/Services/URLcreator.tsx
@@ -1,28 +1,28 @@
-const apiUrl = "https://localhost:5001";
+const apiUrl = process.env.REACT_APP_API_URL
 
-export const getRegisterClientURL = (): string => apiUrl + "/Client/register";
+export const getRegisterClientURL = (): string => apiUrl + '/Client/register'
 
-export const getLoginClientURL = (): string => apiUrl + "/Client/login";
+export const getLoginClientURL = (): string => apiUrl + '/Client/login'
 
-export const getDietsURL = (): string => apiUrl + "/Diets";
+export const getDietsURL = (): string => apiUrl + '/Diets'
 
-export const getMealsURL = (): string => apiUrl + "/Meals";
+export const getMealsURL = (): string => apiUrl + '/Meals'
 
 export const getDietDetailsURL = (dietId: string): string =>
-  apiUrl + "/Diets/" + dietId;
+  apiUrl + '/Diets/' + dietId
 
 export const putDietDetailsURL = (dietId: string): string =>
-  apiUrl + "/Diets/" + dietId;
+  apiUrl + '/Diets/' + dietId
 
 export const getMealDetailsURL = (mealId: string): string =>
-  apiUrl + "/Meals/" + mealId;
+  apiUrl + '/Meals/' + mealId
 
 export const putMealDetailsURL = (mealId: string): string =>
-  apiUrl + "/Meals/" + mealId;
+  apiUrl + '/Meals/' + mealId
 
-export const getClientProfileURL = (): string => apiUrl + "/Client/account";
+export const getClientProfileURL = (): string => apiUrl + '/Client/account'
 
-export const getProducerOrdersURL = (): string => apiUrl + "/Producer/orders";
+export const getProducerOrdersURL = (): string => apiUrl + '/Producer/orders'
 
 export const postCompleteOrderURL = (orderId: string) =>
-  apiUrl + "/Producer/orders/" + orderId + "/complete";
+  apiUrl + '/Producer/orders/' + orderId + '/complete'
